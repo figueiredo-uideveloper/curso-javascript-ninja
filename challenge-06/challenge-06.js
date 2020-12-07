7028e229fafec911da6fea3456037cbe2873fd38
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2020-12-07 14:15:10 -0300

    desafio 06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Brasiliro';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['Fluminense', 'Vasco', 'Flamengo', 'Palmeiras', 'Atletico'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +33,36 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(num) {
+    var teamIndex = num - 1;
+    var maxPositions = 5;
+
+    if(num > maxPositions) {
+        return 'Não temos a informação do time que está nessa posição.';
+    }
+
+    return 'O time que está em ' + num + 'º lugar é o ' + teams[teamIndex] + '.';
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log(showTeamPosition(1)); //O time que está em 1º lugar é o Fluminense.
+console.log(showTeamPosition(2)); //O time que está em 2º lugar é o Vasco.
+console.log(showTeamPosition(3)); //O time que está em 3º lugar é o Flamengo.
+console.log(showTeamPosition(4)); //O time que está em 4º lugar é o Palmeiras.
+console.log(showTeamPosition(6)); //Não temos a informação do time que está nessa posição.
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var counter = 20;
+while (counter >= 20 && counter <= 30) {
+    console.log(counter);
+    counter++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +76,36 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(color) {
+    switch (color) {
+        case 'vermelho':
+            console.log('O hexadecimal para a cor ' + color + ' é #FF0000.');
+        break;
+        case 'azul':
+            console.log('O hexadecimal para a cor ' + color + ' é #0000FF.');
+        break;
+        case 'verde':
+            console.log('O hexadecimal para a cor ' + color + ' é #00FF00.');
+        break;
+        case 'amarelo':
+            console.log('O hexadecimal para a cor ' + color + ' é #FFFF00.');
+        break;
+        case 'preto':
+            console.log('O hexadecimal para a cor ' + color + ' é #000000.');
+        break;
+        default:
+            console.log('Não temos o equivalente hexadecimal para ' + color + '.');
+    }
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+convertToHex('vermelho');
+convertToHex('azul');
+convertToHex('verde');
+convertToHex('amarelo');
+convertToHex('preto');
+convertToHex('roxo');
+convertToHex('laranja');
+convertToHex('branco');
