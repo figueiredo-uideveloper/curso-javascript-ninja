2cb7864ee1ed627317cffeaac4f0d8d6554f8136
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2021-01-15 12:13:28 -0300

    desafio 28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,5 @@
+(function () {
+  'use strict';
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -6,7 +8,7 @@
   "Logradouro, Bairro, Estado, Cidade e CEP." Essas informações serão
   preenchidas com os dados da requisição feita no JS.
   - Crie uma área que receberá mensagens com o status da requisição:
-  "Carregando, sucesso ou erro."
+  "Carregando, successo ou erro."
 
   No JS:
   - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
@@ -25,3 +27,100 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  var $cep = document.querySelector('[data-js="cep"]');
+  var $logradouro = document.querySelector('[data-js="logradouro"]');
+  var $bairro = document.querySelector('[data-js="bairro"]');
+  var $estado = document.querySelector('[data-js="estado"]');
+  var $cidade = document.querySelector('[data-js="cidade"]');
+  var $btnSubmit = document.querySelector('[data-js="submit"]');
+  var $status = document.querySelector('[data-js="status"]');
+
+  var logradouroResponse;
+  var bairroResponse;
+  var estadoResponse;
+  var cidadeResponse;
+
+  var ajax = new XMLHttpRequest();
+  var response;
+
+  ajax.addEventListener( 'readystatechange', function () {
+    if(isRequestLoading()) {
+      statusMessage('loading');
+    }
+
+    if (isRequestDone()) {
+      try {
+        isRequestSuccess('Successo');
+      } catch(error) {
+        isRequestError('CEP inválido');
+      }
+    }
+
+  })
+
+
+  $btnSubmit.addEventListener('click', function (event) {
+    event.preventDefault();
+    var url = cepUrl(
+      "https://ws.apicep.com/cep/[cepCode].[format]",
+      "json",
+      $cep.value
+    );
+
+    ajax.open(
+      "GET",
+      url
+    );
+    ajax.send();
+
+  })
+
+
+
+  function cepUrl(apiUrl, apiFileFormat, cep) {
+    var regex = /\D+/g;
+    var cepOnlyNumbers = cep.replace(regex, "");
+    return apiUrl.replace("[cepCode]", cepOnlyNumbers).replace('[format]', apiFileFormat);
+  }
+
+  function isRequestLoading () {
+    return ajax.readyState !== 4;
+  }
+
+  function isRequestDone() {
+    return ajax.readyState === 4;
+  }
+
+  function isRequestSuccess() {
+    response = JSON.parse(ajax.responseText);
+    fillFields(response);
+    statusMessage('success');
+  }
+
+  function isRequestError() {
+    statusMessage('error');
+  }
+
+  function statusMessage(typeMessage) {
+    var typeMessages = {
+      loading: "Buscando informações para o CEP " + $cep.value + "...",
+      success: "Endereço referente ao CEP " + $cep.value + ".",
+      error: "Não encontramos o endereço para o CEP " + $cep.value + ".",
+    };
+
+    $status.innerHTML = typeMessages[typeMessage];
+  }
+
+  function fillFields(response) {
+    logradouroResponse = response.address;
+    bairroResponse = response.district;
+    estadoResponse = response.state;
+    cidadeResponse = response.city;
+
+    $logradouro.value = logradouroResponse;
+    $bairro.value = bairroResponse;
+    $estado.value = estadoResponse;
+    $cidade.value = cidadeResponse;
+  }
+})();
